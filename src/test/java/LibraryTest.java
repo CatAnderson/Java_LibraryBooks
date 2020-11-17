277591835e114ac2ab653f376cfedd22495cc1b5
CatAnderson <thecatanderson100@gmail.com>
2020-11-17 19:34:26 +0000

    LibraryTest setup

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,14 @@
+import org.junit.Before;
+
 public class LibraryTest {
+
+    private Library library;
+    private Book book;
+
+    @Before
+    public void before(){
+        library = new Library();
+        book = new Book("Do Androids Dream of Electric Sheep?", "Philip K. Dick", "Science Fiction");
+    }
+    
 }
