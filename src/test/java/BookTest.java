6c8100c8e2c9602c19852db6a7db75c7c9091a85
CatAnderson <thecatanderson100@gmail.com>
2020-11-17 19:18:15 +0000

    testing book has title

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,4 +1,7 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class BookTest {
 
@@ -7,6 +10,11 @@ public class BookTest {
     @Before
     public void before(){
         book = new Book("Do Androids Dream of Electric Sheep?", "Philip K. Dick", "Science Fiction")
+    }
+
+    @Test
+    public void hasTitle(){
+        assertEquals("Do Androids Dream of Electric Sheep?", book.title);
     }
 
 }
